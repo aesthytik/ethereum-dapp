af561b1d2b1e7b8200afe86c6708087f422f3534
vipin rawat <rawat.vipin4818@gmail.com>
2021-09-11 13:28:32 +0530

    created token

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,9 +26,9 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    ropsten: {
-      url: "https://ropsten.infura.io/v3/e6f3a6638e8447feb892af9fe7cc7b3e",
-      accounts: [`0x${process.env.REACT_APP_PRV_KEY}`]
-    }
+    // ropsten: {
+    //   url: "https://ropsten.infura.io/v3/e6f3a6638e8447feb892af9fe7cc7b3e",
+    //   accounts: [`0x${process.env.REACT_APP_PRV_KEY}`]
+    // }
   }
 };
